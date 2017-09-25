668f02fb06d1ea9e8d358f3f59eb62a75485b0ab
frank <franciskipchumba5@gmail.com>
2017-09-25 16:43:33 +0300

    push data to api

---
+++ add_feed.py
@@ -0,0 +1,15 @@
+
+
+from urllib import request, parse
+
+
+def create_post():
+  url = 'http://34.207.10.230:3000/posts/'
+  title = input("Enter Post title\n")
+  body = input("Enter body content\n")
+  request_data = {'title': title, 'body': body}
+  data = parse.urlencode(request_data).encode()
+  req = request.Request(url, data=data)
+  resp = request.urlopen(req)
+  print(resp.__dict__)
+create_post()
--- primfeed_post.py
+++ primfeed_post.py
@@ -2,39 +2,10 @@ import urllib.request
 import json 
 
 class PrimeFeedPost(object):
-	def __init__(self, title, body, api_url=None):
+	def __init__(self, title, body):
 		self.title = title
 		self.body = body
-		self.api_url = api_url
 
-	def add_post_feed(title, body):
-		# api url to use
-		api_url = 'http://34.207.10.230:3000/posts'
 
-		# data to be posted
-		payload = {
-				    "title": title,
-				    "body": body
-				  }
-
-		# specify headers
-		headers = {
-					'Content-Type': 'application/json'
-					}
-
-		req = urllib.request.Request(api_url)
-
-		req.add_header(headers)
-
-		jsondata = json.dumps(payload)
-
-		jsondataasbytes= jsondata.encode('utf-8')
-
-		req.add_header('Content-Type', len(jsondataasbytes))
-		print(jsondataasbytes)
-
-		response = urllib.request.urlopen(req, jsondataasbytes)
-
-		# response = requests.post(url, data=json.dumps(payload), headers=headers)
-
-		Return ( response )
+	def get_post():
+		return self.title
