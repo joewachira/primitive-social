db0c7182055ac3eae4c4ef2ace9a7e96445790a0
Stephen Muthama <muthamass@gmail.com>
2017-09-25 15:42:14 +0300

    check name and body not blank

--- test_comments.py
+++ test_comments.py
@@ -3,7 +3,10 @@ import unittest
 class TddInComments(unittest.TestCase):
 
     def setup(self):
-        self.comments = Comment()
+        self.com = Comment()
+
+    def test_comments_create_method_is_not_black(self):
+        self.assertRaises(ValueError, self.com.create, '', '')
 
 if __name__ =='__main__':
     unittest.main(test)        
