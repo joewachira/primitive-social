4800938aa119c1054174a6f373e6beff33fe3b71
gr1d99 <gideonkimutai9@gmail.com>
2017-09-25 15:53:31 +0300

    Add a get_comment method

--- comments.py
+++ comments.py
@@ -8,21 +8,20 @@ class Comment(object):
         self.body = None
 
     def create(self, commentId, postId, user, body):
-
         """
         A function to create comment, returns True if successful
         """
         if not isinstance(commentId, int):  # check if commentId is an integer
-            raise ValueError("Expected value of %(xtype)s" % dict(xtype=type(int)))
+            raise ValueError("Expected value of type %(xtype)s" % dict(xtype=int.__name__))
 
         if not isinstance(postId, int):  # check if postId is an integer
-            raise ValueError("Expected value of %(xtype)s" % dict(xtype=type(int)))
+            raise ValueError("Expected value of type %(xtype)s" % dict(xtype=int.__name__))
 
         if not isinstance(user, str):  # check if user is str
-            raise ValueError("Expected argument of %(xtype)s" % dict(xtype=type(str)))
+            raise ValueError("Expected argument of type %(xtype)s" % dict(xtype=str.__name__))
 
         if not isinstance(body, str):  # check if body is str
-            raise ValueError("Expected argument of %(xtype)s" % dict(xtype=type(str)))
+            raise ValueError("Expected argument of type %(xtype)s" % dict(xtype=str.__name__))
 
         self.commentId = commentId
         self.postId = postId
