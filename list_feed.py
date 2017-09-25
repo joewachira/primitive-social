b8b2d8e4894219b0bb1a8c0cc06ccd5582a80e39
frank <franciskipchumba5@gmail.com>
2017-09-25 16:50:37 +0300

    list all feeds

--- list_feed.py
+++ list_feed.py
@@ -1,10 +1,8 @@
-
-
 from urllib import request, parse
 
 
 def create_post():
-  url = 'http://34.207.10.230:3000/posts/'
+  url = 'http://34.207.10.230:3000/posts'
   title = input("Enter Post title\n")
   body = input("Enter body content\n")
   request_data = {'title': title, 'body': body}
